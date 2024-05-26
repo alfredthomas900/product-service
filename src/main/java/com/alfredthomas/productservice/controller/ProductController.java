f44876c0330f6d62caf4a90927395fb23d480ca2
Alfred Thomas <alfredthomas900@gmail.com>
2024-05-26 13:47:10 +0530

    Product Service - Added 2 APIs for creating product in MongoDB and fetching all products from MongoDB

---
+++ src/main/java/com/alfredthomas/productservice/controller/ProductController.java
@@ -0,0 +1,39 @@
+package com.alfredthomas.productservice.controller;
+
+import com.alfredthomas.productservice.dto.ProductRequest;
+import com.alfredthomas.productservice.dto.ProductResponse;
+import com.alfredthomas.productservice.service.ProductService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping(value = "/api/product")
+@RequiredArgsConstructor
+@Slf4j
+public class ProductController {
+
+  private final ProductService productService;
+
+  @PostMapping
+  @ResponseStatus(HttpStatus.CREATED)
+  public void createProduct(@RequestBody ProductRequest productRequest) {
+    log.debug("createProduct started for request : {}", productRequest);
+    productService.createProduct(productRequest);
+  }
+
+  @GetMapping
+  @ResponseStatus(HttpStatus.OK)
+  public List<ProductResponse> getAllProducts() {
+    log.debug("getAllProducts started");
+    return productService.getAllProducts();
+  }
+}
